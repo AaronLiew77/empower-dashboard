5d72e54a53dc3a456028af43f1a0e8cbe44639df
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-06-24 01:00:19 +0800

    Update BenefitCard.jsx

--- src/components/BenefitCard.jsx
+++ src/components/BenefitCard.jsx
@@ -4,7 +4,7 @@ import RadialBarChart from '../components/RadialBarChart';
 const BenefitCard = ({ chartData, label}) =>  {
   return (
       <div className="petrol-rounded-box bg-[#697CE0] border border-[#5C97F0] rounded-xl pb-3">
-        <h3 className='text-md text-[#FFFFFF] text-center font-bold mt-4 text-[#2B69F5]'>{label}</h3>
+        <h3 className='text-md text-[#FFFFFF] text-center font-bold mt-4 text-[#FFFFFF]'>{label}</h3>
         <div className="bg-[#FFFFFF] rounded-xl w-10/12 m-auto">
           <RadialBarChart chartData={chartData} label='% Utilized' color='#2B69F5'/>
         </div>
