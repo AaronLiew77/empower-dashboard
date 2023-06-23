f69dbed0ea71a8f13a5fca88463cc1cc0f3997dc
AaronLiew77 <aaronliewjunwei@gmail.com>
2023-06-23 23:00:54 +0800

    a

--- src/components/DashboardSlider.jsx
+++ src/components/DashboardSlider.jsx
@@ -5,8 +5,14 @@ import {
     BellIcon,
 } from "@heroicons/react/24/solid";
 
+import BenefitCard from "../components/BenefitCard";
+import RadialBarChart from '../components/RadialBarChart';
+
+
+
 export default function DashboardSlider({ employeeName, onClose }) {
     const [open, setOpen] = useState(true)
+    const chartData = [32.1]
 
     return (
         <Transition.Root show={open} as={Fragment}>
@@ -55,7 +61,7 @@ export default function DashboardSlider({ employeeName, onClose }) {
                                             </button>
                                         </div>
                                     </Transition.Child>
-                                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl rounded-3xl">
+                                    <div className="flex h-full flex-col bg-white py-6 shadow-xl rounded-3xl overflow-y-auto">
                                         <div className="px-4 sm:px-6">
                                             <Dialog.Title style={{ fontSize: '1.5em' }} className="text-base font-semibold leading-6 text-gray-900">
                                                 Employee Information
@@ -80,11 +86,83 @@ export default function DashboardSlider({ employeeName, onClose }) {
                                             </div>
 
                                         </div>
-                                        <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl rounded-3xl mt-4">
-                                            <div className="px-4 sm:px-6">
-                                                <Dialog.Title style={{ fontSize: '1.5em' }} className="text-base font-semibold leading-6 text-gray-900">
-                                                    Expenses and Benefits Utilization
-                                                </Dialog.Title>
+                                        <div className="px-4 sm:px-6">
+                                            <Dialog.Title style={{ fontSize: '1.5em' }} className="text-base font-semibold leading-6 text-gray-900">
+                                                Expenses and Benefits Utilization
+                                            </Dialog.Title>
+                                            <p className='mb-4'>Benefit Type: Flexi Level 2</p>
+                                        </div>
+
+
+                                        <div className='flex px-4 overflow'> {/* Added 'overflow-hidden' class */}
+                                            <div className='w-1/2 rounded-xl border border-[#5C97F0]'>
+                                                <h3 className='text-md text-center font-bold mt-4 text-[#2B69F5]'>Total Utilization</h3>
+                                                <RadialBarChart chartData={chartData} label='% Utilized' color='#2B69F5' />
+                                            </div>
+                                            <div className='w-1/2 bg-[#9087FB] ml-2 rounded-xl flex flex-col p-4'>
+                                                <h3 className='text-white text-sm font-bold'>Utilized</h3>
+                                                <h3 className='text-white text-sm'>RM 321.00</h3>
+                                                <br></br>
+                                                <h3 className='text-white text-sm font-bold'>Card Limit</h3>
+                                                <h3 className='text-white text-sm'>RM 1000.00</h3>
+                                            </div>
+                                        </div>
+
+                                        <div className='flex'>
+                                            <div className='w-1/2 rounded-xl border border-[#5C97F0]'>
+
+                                                <BenefitCard chartData={[30]} label="Petrol Allowance" />
+                                            </div>
+                                            <div className='w-1/2 rounded-xl border border-[#5C97F0]'>
+
+                                                <BenefitCard chartData={[40]} label="Gym Allowance" />
+                                            </div>
+                                            <div className='w-1/2 rounded-xl border border-[#5C97F0]'>
+
+                                                <BenefitCard chartData={[70]} label="Flexi Benefits" />
+                                            </div>
+                                        </div>
+
+                                        <div className="px-4 sm:px-6 mt-2">
+                                            <Dialog.Title style={{ fontSize: '1.5em' }} className="text-base font-semibold leading-6 text-gray-900 mt-2">
+                                                Recent Transactions
+                                            </Dialog.Title>
+                                        </div>
+                                        <div class="flex flex-col">
+                                            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                                <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                                                    <div class="overflow-hidden">
+                                                        <table class="min-w-full text-left text-sm font-light">
+                                                            <thead class="border-b font-medium dark:border-neutral-500">
+                                                                <tr>
+                                                                    <th scope="col" class="px-6 py-4">Transaction</th>
+                                                                    <th scope="col" class="px-6 py-4">Category</th>
+                                                                    <th scope="col" class="px-6 py-4">Amount</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <tr
+                                                                    class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                                                                    <td class="whitespace-nowrap px-6 py-4">Grab Taxi <br></br> <span className='text-sm font-light text-gray-500'>9:14P.M.</span></td>
+                                                                    <td class="whitespace-nowrap px-6 py-4">Transport Allowance</td>
+                                                                    <td class="whitespace-nowrap px-6 py-4 text-red-500">-RM23.40</td>
+                                                                </tr>
+                                                                <tr
+                                                                    class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                                                                    <td class="whitespace-nowrap px-6 py-4">Grab Taxi <br></br> <span className='text-sm font-light text-gray-500'>9:14P.M.</span></td>
+                                                                    <td class="whitespace-nowrap px-6 py-4">Transport Allowance</td>
+                                                                    <td class="whitespace-nowrap px-6 py-4 text-red-500">-RM23.40</td>
+                                                                </tr>
+                                                                <tr
+                                                                    class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                                                                    <td class="whitespace-nowrap px-6 py-4">Grab Taxi <br></br> <span className='text-sm font-light text-gray-500'>9:14P.M.</span></td>
+                                                                    <td class="whitespace-nowrap px-6 py-4">Transport Allowance</td>
+                                                                    <td class="whitespace-nowrap px-6 py-4 text-red-500">-RM23.40</td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
 
